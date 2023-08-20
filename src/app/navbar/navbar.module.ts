3b0185c406cd42a6f8a063a9bc693c666b411022
krishkarmyal07 <krishkarmyal96@gmail.com>
2023-08-21 01:31:58 +0530

    first commit

---
+++ src/app/navbar/navbar.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar.component';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+
+
+@NgModule({
+  declarations: [NavbarComponent],
+  imports: [
+    CommonModule,
+    MatMenuModule,
+    MatButtonModule,
+  ]
+})
+export class NavbarModule { }
+ 
+
